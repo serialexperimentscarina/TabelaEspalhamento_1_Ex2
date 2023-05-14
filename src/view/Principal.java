6087ed561bc7215bccc6a98cb794ace96250d810
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-05-13 23:21:20 -0300

    refactor: improve Principal with single try-catch and variable declaration

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -9,60 +9,51 @@ public class Principal {
 
 	public static void main(String[] args) {
 		TabelaMoradorController tabela = new TabelaMoradorController(10);
-		String input;
+		String input, nome, modelo, cor, placa;
+		int numApto, andar;
 		
-		do {
-			input = JOptionPane.showInputDialog("MENU: \n1 - Adicionar morador \n2 - Remover morador \n3 - Buscar morador \n4 - Listar moradores de um andar \n5 - Sair", "");
-			if (input == null) {
-				JOptionPane.showMessageDialog(null, "Encerrando.");
-				System.exit(0);
-			} else {
-				switch (input) {
-				case("1"):
-					try {
-						int numApto;
+		try {
+			do {
+				input = JOptionPane.showInputDialog("MENU: \n1 - Adicionar morador \n2 - Remover morador \n3 - Buscar morador \n4 - Listar moradores de um andar \n5 - Sair", "");
+				if (input == null) {
+					JOptionPane.showMessageDialog(null, "Encerrando.");
+					System.exit(0);
+				} else {
+					switch (input) {
+					
+					case("1"):
 						do {
 							numApto = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do apto: "));
 							if (numApto < 100 || numApto > 1999) {
 								JOptionPane.showMessageDialog(null, "Número de apartamento inválido, tente novamente", "Erro", JOptionPane.ERROR_MESSAGE);
 							}
 						} while (numApto < 100 || numApto > 1999);
-						String nome = JOptionPane.showInputDialog("Digite o nome do morador: ");
-						String modelo = JOptionPane.showInputDialog("Digite o modelo do carro: ");
-						String cor = JOptionPane.showInputDialog("Digite a cor do carro: ");
-						String placa = JOptionPane.showInputDialog("Digite a placa do carro: ");
+						nome = JOptionPane.showInputDialog("Digite o nome do morador: ");
+						modelo = JOptionPane.showInputDialog("Digite o modelo do carro: ");
+						cor = JOptionPane.showInputDialog("Digite a cor do carro: ");
+						placa = JOptionPane.showInputDialog("Digite a placa do carro: ");
 						tabela.adiciona(new Morador(numApto, nome, modelo, cor, placa));
-					} catch (Exception e) {
-						JOptionPane.showMessageDialog(null, "Ocorreu um erro ao tentar adicionar o morador", "Erro", JOptionPane.ERROR_MESSAGE);
-						e.printStackTrace();
-					}
-					break;
-				case("2"):
-					try {
-						int numApto;
+						break;
+						
+					case("2"):
 						do {
 							numApto = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do apto: "));
 							if (numApto < 100 || numApto > 1999) {
 								JOptionPane.showMessageDialog(null, "Número de apartamento inválido, tente novamente", "Erro", JOptionPane.ERROR_MESSAGE);
 							}
 						} while (numApto < 100 || numApto > 1999);
-						String nome = JOptionPane.showInputDialog("Digite o nome do morador: ");
+						nome = JOptionPane.showInputDialog("Digite o nome do morador: ");
 						tabela.remove(new Morador(numApto, nome, null, null, null));
-					} catch (Exception e) {
-						JOptionPane.showMessageDialog(null, "Ocorreu um erro ao tentar remover o morador", "Erro", JOptionPane.ERROR_MESSAGE);
-						e.printStackTrace();
-					}
-					break;
-				case("3"):
-					try {
-						int numApto;
+						break;
+						
+					case("3"):
 						do {
 							numApto = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do apto: "));
 							if (numApto < 100 || numApto > 1999) {
 								JOptionPane.showMessageDialog(null, "Número de apartamento inválido, tente novamente", "Erro", JOptionPane.ERROR_MESSAGE);
 							}
 						} while (numApto < 100 || numApto > 1999);
-						String nome = JOptionPane.showInputDialog("Digite o nome do morador: ");
+						nome = JOptionPane.showInputDialog("Digite o nome do morador: ");
 						Morador moradorBuscado = tabela.busca(new Morador(numApto, nome, null, null, null));
 						
 						if(moradorBuscado != null) {
@@ -70,14 +61,9 @@ public class Principal {
 						} else {
 							JOptionPane.showMessageDialog(null, "Morador não encontrado", "Erro", JOptionPane.ERROR_MESSAGE);
 						}
-					} catch (Exception e) {
-						JOptionPane.showMessageDialog(null, "Ocorreu um erro ao tentar buscar o morador", "Erro", JOptionPane.ERROR_MESSAGE);
-						e.printStackTrace();
-					}
-					break;
-				case("4"):
-					try {
-						int andar;
+						break;
+						
+					case("4"):
 						do {
 							andar = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do andar: "));
 							if (andar < 1 || andar > 10) {
@@ -85,20 +71,20 @@ public class Principal {
 							}
 						} while (andar < 1 || andar > 10);
 						tabela.lista(andar);
-					} catch (Exception e) {
-						JOptionPane.showMessageDialog(null, "Ocorreu um erro ao tentar listar este andar", "Erro", JOptionPane.ERROR_MESSAGE);
-						e.printStackTrace();
+						break;
+						
+					case("5"):
+						JOptionPane.showMessageDialog(null, "Encerrando.");
+						break;
+					default:
+						JOptionPane.showMessageDialog(null, "Opção não reconhecida", "Erro", JOptionPane.ERROR_MESSAGE);
+						break;
 					}
-					break;
-				case("5"):
-					JOptionPane.showMessageDialog(null, "Encerrando.");
-					break;
-				default:
-					JOptionPane.showMessageDialog(null, "Opção não reconhecida", "Erro", JOptionPane.ERROR_MESSAGE);
-					break;
 				}
-			}
-		} while (!input.equals("5"));
+			} while (!input.equals("5"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
